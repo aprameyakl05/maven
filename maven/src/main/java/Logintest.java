ed9275f59a5fffa5e4061d6446a4fd9132725ac9
aprameyakl05 <aprameyakl@gmail.com>
2025-05-05 09:18:35 +0530

    Add files via upload

---
+++ maven/src/main/java/Logintest.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Logintest {
+    public static void main(String[] args) throws InterruptedException{
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/v1/");
+        driver.manage().window().maximize();
+        Thread.sleep(1000);
+
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        Thread.sleep(1000);
+
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        Thread.sleep(1000);
+
+        driver.findElement(By.id("login-button")).click();
+        Thread.sleep(1000);
+
+        driver.quit();
+    }
+}
